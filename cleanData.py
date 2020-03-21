c72be5d646d5831764a61832d8aac925a9d30045
Latha <lathakrishnamoorthy89@gmail.com>
2020-03-20 22:18:43 -0500

    Loaded Data and did some analysis of data

--- cleanData.py
+++ cleanData.py
@@ -16,19 +16,26 @@ class cleanData:
     def loadData(self, filename):
         print('Reading', filename)
         self.df_combined_file = (pd.read_csv(path + filename,index_col=False))
+
+        # commented to reduce prints
+        '''
         print(self.df_combined_file.head())
         print(self.df_combined_file.shape)
+        '''
 
 
     def identify_columns_with_missing_values(self):
+        # commented to reduce prints
+        '''
         print(list(self.df_combined_file.columns))
         print(self.df_combined_file.isna)
         #print((self.df_combined_file.columns[self.df_combined_file.isna().any()]).count)
+
         # Printing those column names where there is atleast one missing value
         print(dict(self.df_combined_file.isna().any()))
 
         print('Dimensions before removal are ',self.df_combined_file.shape)
-
+        '''
         # Find the number of rows with empty values in all columns and dropping those
         self.df_combined_file.dropna(axis=0, how='all',inplace=True)
         print('Dimensions after removal are ',self.df_combined_file.shape)
@@ -48,16 +55,19 @@ class cleanData:
         print((len
                (list(self.df_combined_file['CITY'].unique()))
                ))
+
+        # commented to reduce prints
+        '''
         print(self.df_combined_file.describe())
 
         print(self.df_combined_file.info())
+        '''
 
     def corr_plots_price_lat_long(self):
         corr = self.df_combined_file[['PRICE','LATITUDE','LONGITUDE']].corr()
         print(corr)
-
-
-
+        # commented to reduce prints
+        '''
         ax = sns.heatmap(
             corr,
             annot=True,
@@ -67,16 +77,21 @@ class cleanData:
         )
 
         plt.show()
+        '''
 
     def data_price_yearbuilt(self):
         temp_df=self.df_combined_file[['PRICE','YEAR BUILT']]
         temp_df1=temp_df.dropna()
+
+        # commented to reduce prints
+        '''
         print(temp_df1.head())
         print(temp_df1.shape)
-
+        '''
         corr = temp_df1.corr()
         print(corr)
-
+        # commented to reduce prints
+        '''
         ax = sns.heatmap(
             corr,
             annot=True,
@@ -86,6 +101,190 @@ class cleanData:
         )
 
         plt.show()
+        '''
+
+    def data_price_age(self):
+        temp_df = self.df_combined_file[['PRICE', 'YEAR BUILT']]
+        temp_df1 = temp_df.dropna()
+        minYear=min(temp_df1['YEAR BUILT'])
+        maxYear = max(temp_df1['YEAR BUILT'])
+        print("min Year is " ,minYear)
+        print("max Year is ", maxYear)
+        print(list(temp_df1['YEAR BUILT'].unique()))
+        temp_df1.columns = [column.replace(" ", "_") for column in temp_df1.columns]
+        temp_df1=temp_df1.loc[temp_df1['YEAR_BUILT'] <1800.0 , ['YEAR_BUILT']]
+
+        # commented to reduce prints
+        '''
+        print(temp_df1)
+        print(temp_df1.shape)
+        '''
+        years=[1060,1055,1500,1025]
+        self.df_combined_file.columns = [column.replace(" ", "_") for column in self.df_combined_file.columns]
+        print(self.df_combined_file.query('YEAR_BUILT==1060.0'))
+
+        # creating additional column to the dataframe
+        self.df_combined_file['AGE']=2021-self.df_combined_file['YEAR_BUILT']
+
+        # commented to reduce prints
+        '''
+        print(self.df_combined_file)
+        print(self.df_combined_file.shape)
+        
+        scatter_plot_price_year=self.df_combined_file.plot.scatter(x='YEAR_BUILT',y='PRICE')
+        plt.show()
+        
+        '''
+
+        self.df_combined_file = self.df_combined_file[self.df_combined_file.YEAR_BUILT >= 1800.0]
+        self.df_combined_file =self.df_combined_file.reset_index(drop=True)
+
+        temp_df = self.df_combined_file[['PRICE', 'AGE']]
+        temp_df1 = temp_df.dropna()
+
+        # commented to reduce prints
+        '''
+        print(temp_df1)
+        print(temp_df1.shape)
+        '''
+        corr = temp_df1.corr()
+        print(corr)
+        # commented to reduce prints
+        '''
+        ax = sns.heatmap(
+            corr,
+            annot=True,
+            vmin=-1, vmax=1, center=0,
+            cmap=sns.diverging_palette(20, 220, n=200),
+            square=True
+        )
+
+        plt.show()
+        
+        print("After removing age related columns")
+        print(self.df_combined_file.describe())
+        '''
+
+    def clean_PropertyType(self):
+
+        print()
+        print(list(self.df_combined_file['PROPERTY_TYPE'].unique()))
+        propertyTypes = ['Condo/Co-op', 'Single Family Residential', 'Multi-Family (2-4 Unit)', 'Townhouse','Multi-Family (5+ Unit)']
+        self.df_combined_file = self.df_combined_file [self.df_combined_file.PROPERTY_TYPE.isin(propertyTypes)]
+        self.df_combined_file = self.df_combined_file.reset_index(drop=True)
+        print(list(self.df_combined_file['PROPERTY_TYPE'].unique()))
+        #commented to reduce prints
+        '''
+        print(self.df_combined_file)
+        print(self.df_combined_file.shape)
+        print(self.df_combined_file.describe())
+        '''
+
+        propertyTypes_Multi=['Multi-Family (2-4 Unit)', 'Multi-Family (5+ Unit)']
+        new_datafram_MultiFamily=self.df_combined_file[self.df_combined_file.PROPERTY_TYPE.isin(propertyTypes_Multi)]
+
+        # commented to reduce prints
+        '''
+        print(new_datafram_MultiFamily)
+        print(new_datafram_MultiFamily.shape)
+        print(list(new_datafram_MultiFamily['PROPERTY_TYPE'].unique()))
+        print(new_datafram_MultiFamily.describe())
+        '''
+
+    def keep_necessary_columns(self):
+        print(self.df_combined_file.shape)
+        print(self.df_combined_file.columns)
+        col_to_drop=['SALE_TYPE','SOLD_DATE','ADDRESS','DAYS_ON_MARKET','NEXT_OPEN_HOUSE_START_TIME','INTERESTED','FAVORITE','SOURCE','MLS#','SOURCE','LOT_SIZE','$/SQUARE_FEET','HOA/MONTH', 'STATUS', 'j','NEXT_OPEN_HOUSE_END_TIME','_']
+        self.df_combined_file=self.df_combined_file.drop(labels=col_to_drop,axis=1)
+
+        # commented to reduce prints
+        '''
+        print(self.df_combined_file.columns)
+        print(self.df_combined_file.shape)
+        print(self.df_combined_file.describe())
+        '''
+        #PRICE, PROPERTY_TYPE, ZIP, BEDS, BATHS, SQUARE_FEET, LOCATION, YEAR_BUILT, URL, LATITUDE, LONGITUDE
+
+    def remove_duplicates(self):
+        print('Before removing duplicates')
+        print(self.df_combined_file.shape)
+        self.df_combined_file.drop_duplicates(keep='first',inplace=True,ignore_index=True)
+        print('After removing duplicates')
+        print(self.df_combined_file.shape)
+
+    def beds_Baths(self):
+
+        #to find the BEDS calculation removing rows where there are no value of BEDS
+
+        # commented to reduce prints
+        print()
+        '''
+        print('After checking BEDS')
+        print(list(self.df_combined_file['BEDS'].unique()))
+        '''
+
+        self.df_combined_file=self.df_combined_file [~self.df_combined_file.BEDS.isna()]
+        print(self.df_combined_file)
+
+        # Find calculations for Baths removing rows where there are no value of BATHS
+        print('After checking BATHS')
+        print(list(self.df_combined_file['BATHS'].unique()))
+        self.df_combined_file = self.df_combined_file[~self.df_combined_file.BATHS.isna()]
+        print(self.df_combined_file.shape)
+        # commented to reduce prints
+        '''
+        print(self.df_combined_file )
+        print(self.df_combined_file .shape)
+        print(list(self.df_combined_file ['BATHS'].unique()))
+        '''
+        print(list(self.df_combined_file['BEDS'].unique()))
+
+
+        # commented to reduce prints
+        '''
+     #Histogram of Beds and Baths
+     self.df_combined_file['BEDS'].plot.hist()
+     self.df_combined_file['BATHS'].plot.hist()
+     plt.show()
+    
+    
+     # Printing those column names where there is atleast one missing value
+     print(dict(self.df_combined_file.isna().any()))
+    '''
+    def clean_sqft(self):
+        # creating a new dataframe with data with valid sqft values;
+
+        print(self.df_combined_file.shape)
+
+        new_df_with_sqft_value=self.df_combined_file[~self.df_combined_file.SQUARE_FEET.isna()]
+        print('sqft with value')
+        print(new_df_with_sqft_value.shape)
+
+        new_df_with_sqft_na = self.df_combined_file[self.df_combined_file.SQUARE_FEET.isna()]
+        print('sqft without value')
+        print(new_df_with_sqft_na.shape)
+
+        temp_df = new_df_with_sqft_value[['BEDS', 'BATHS','SQUARE_FEET']]
+
+        corr = temp_df.corr()
+        print(corr)
+
+        print('Unique num of beds in self.df_combined_file')
+        print(list(self.df_combined_file['BEDS'].unique()))
+        print('Unique num of beds in new_df_with_sqft_value')
+        print(list(new_df_with_sqft_value['BEDS'].unique()))
+        print('Unique num of beds in nnew_df_with_sqft_na')
+        print(list(new_df_with_sqft_na['BEDS'].unique()))
+
+        print('Unique num of BATHS in self.df_combined_file')
+        print(list(self.df_combined_file['BATHS'].unique()))
+        print('Unique num of BATHS in new_df_with_sqft_value')
+        print(list(new_df_with_sqft_value['BATHS'].unique()))
+        print('Unique num of BATHS in new_df_with_sqft_na')
+        print(list(new_df_with_sqft_na['BATHS'].unique()).sort())
+
+
+        # find the mean sqft value of
 
 def main():
     print("inside Main")
@@ -93,9 +292,15 @@ def main():
     obj.loadData("combined.csv")
     obj.identify_columns_with_missing_values()
     obj.uniqueValues()
-    obj.coorelation_plots()
+    obj.corr_plots_price_lat_long()
     obj.data_price_yearbuilt()
+    obj.data_price_age()
 
+    obj.clean_PropertyType()
+    obj.keep_necessary_columns()
+    obj.remove_duplicates()
+    obj.beds_Baths()
+    obj.clean_sqft()
 
 if __name__ == '__main__':
     main()
